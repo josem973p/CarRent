9e8783a2606b82f1e28e8aea1077c5f7f3097467
manuel <josscas1997@gmail.com>
2021-08-10 13:17:51 -0500

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PrincipalComponent } from './paginas/principal/principal.component';
+import { DetalleComponent } from './paginas/detalle/detalle.component';
+import { PagoComponent } from './paginas/pago/pago.component';
+import { DetallePagoComponent } from './paginas/detalle-pago/detalle-pago.component';
+//import { PrincipalComponent } from './app/paginas/principal/principal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrincipalComponent,
+    DetalleComponent,
+    PagoComponent,
+    DetallePagoComponent
   ],
   imports: [
     BrowserModule,
